d00c00f4b21b5ba38fa2b76ad99b29538d6cdf94
ENSOO1 <cafenetjana@gmail.com>
2026-09-06 19:00:00 +0330

    Create data.js

---
+++ data.js
@@ -0,0 +1,14 @@
+const transactions = [
+  {
+    customer: "علی",
+    service: "پرینت",
+    amount: 150000,
+    method: "کارتخوان"
+  },
+  {
+    customer: "رضا",
+    service: "ثبت‌نام",
+    amount: 300000,
+    method: "نقدی"
+  }
+];
